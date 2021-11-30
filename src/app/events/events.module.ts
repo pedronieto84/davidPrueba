45f29ffc1574734d4a761db2d7f7712d31f15b90
David Castrillón <david.castri@hotmail.es>
2021-11-30 16:33:42 +0100

    Modelo de event-card hecho

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -3,21 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { EventsRoutingModule } from './events-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { BillboardComponent } from './pages/billboard/billboard.component';
 import { EventDetailComponent } from './pages/event-detail/event-detail.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
+
 
 
 
 @NgModule({
   declarations: [
     BillboardComponent,
-    EventDetailComponent
+    EventDetailComponent,
+    EventCardComponent
   ],
   imports: [
     CommonModule,
     EventsRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule
   ]
 })
 export class EventsModule { }
